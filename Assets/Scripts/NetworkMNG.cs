227335a0bcc01f8c9e680ba1d48e1ca6260f3218
DESKTOP-9QHNJH3\NporVerse_230220 <tmdqlszld123@naver.com>
2023-09-12 11:18:36 +0900

    주소 쓸데 없는거 변경

--- Assets/Scripts/NetworkMNG.cs
+++ Assets/Scripts/NetworkMNG.cs
@@ -21,48 +21,18 @@ public class NetworkMNG : MonoBehaviour
 
     private static string _serverURL = "";
 
-    private static string _releaseServerURL = "http://puzzle-sugar-flavor.com:80/sugarmonster/public_html/index.php/";
+    private static string _releaseServerURL = "http://114.108.130.128/sugarmonster/public_html/index.php/";
     private static string _devServerURL = "http://114.108.130.127:80/sugarmonster/public_html/index.php/";
 
     private static string _serverMapDataURL = "";
 
-    private static string _releaseServerMapDataURL = "http://puzzle-sugar-flavor.com:80/sugarmonster/public_html/map_data/";
+    private static string _releaseServerMapDataURL = "http://114.108.130.128/sugarmonster/public_html/map_data/";
     private static string _devServerMapDataURL = "http://114.108.130.127:80/sugarmonster/public_html/map_data/";
 
+    public static string mapediterURL = "http://114.108.130.127:80/sugarmonster/public_html/map_data/map_editer/";
 
-    public static string mapediterURL = "http://puzzle-sugar-flavor.com/sugarmonster/public_html/map_data/map_editer/";
-
-    public string gachaRateUrl = "https://puzzle-sugar-flavor.com/sugarmonster_admin/public_html/index.php/admin/gacha_rate";
-    public string gachaSpecialRateUrl = "https://puzzle-sugar-flavor.com/sugarmonster_admin/public_html/index.php/admin/gacha_special_rate";
-
-    public string gachaRankingRateUrl = "https://puzzle-sugar-flavor.com/sugarmonster_admin/public_html/index.php/admin/gacha_ranking_rate";
-
-    public string leagueRewardUrl = "https://puzzle-sugar-flavor.com/sugarmonster_admin/public_html/index.php/admin/league_reward";
-
-
-    public string termsUrl = "https://puzzle-sugar-flavor.com/terms/terms_of_use.html";
-
-    public string termsViewKrUrl = "http://puzzle-sugar-flavor.com/sugarmonster_admin/public_html/index.php/admin/a_terms_view_kr";
-    public string termsViewEnUrl = "http://puzzle-sugar-flavor.com/sugarmonster_admin/public_html/index.php/admin/a_terms_view_en";
-
-    public string termsInfoViewKrUrl = "http://puzzle-sugar-flavor.com/sugarmonster_admin/public_html/index.php/admin/a_terms_view_kr/info";
-    public string termsInfoViewEnUrl = "http://puzzle-sugar-flavor.com/sugarmonster_admin/public_html/index.php/admin/a_terms_view_en/info";
-
-    public static string resourceBundleBaseUrl = "";
-    public static string rankingBundleBaseUrl = "";
-
-#if UNITY_EDITOR || UNITY_ANDROID
-    public static string resourceBundleReleaseUrl = "http://puzzle-sugar-flavor.com/sugarmonster/public_html/app_assets/android/";
-    public static string resourceBundleDevUrl = "http://114.108.130.127/sugarmonster/public_html/app_assets/android/";
-#else
-    public static string resourceBundleReleaseUrl   = "http://puzzle-sugar-flavor.com/sugarmonster/public_html/app_assets/ios/";
-    public static string resourceBundleDevUrl       = "http://114.108.130.127/sugarmonster/public_html/app_assets/ios/";
-#endif
-    public static string rankingBundleReleaseUrl = "http://puzzle-sugar-flavor.com/sugarmonster/public_html/app_assets/ranking/";
-    public static string rankingBundleDevUrl = "http://114.108.130.127/sugarmonster/public_html/app_assets/ranking/";
 
     public delegate void NetworkResultCallback(string result);
-    public delegate void ImageCallback(Texture texture);
 
     public const string NETWORK_ERROR = "networkError";
 
@@ -80,16 +50,11 @@ public class NetworkMNG : MonoBehaviour
         {
             _serverURL = _devServerURL;
             _serverMapDataURL = _devServerMapDataURL;
-            resourceBundleBaseUrl = resourceBundleDevUrl;
-            rankingBundleBaseUrl = rankingBundleDevUrl;
         }
         else
         {
             _serverURL = _releaseServerURL;
             _serverMapDataURL = _releaseServerMapDataURL;
-
-            resourceBundleBaseUrl = resourceBundleReleaseUrl;
-            rankingBundleBaseUrl = rankingBundleReleaseUrl;
         }
     }
 
